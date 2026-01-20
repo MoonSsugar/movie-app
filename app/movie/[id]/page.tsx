49a57227877f74aabb6b7e557fa8b1bae0abfe3a
MoonSsugar <hirnyjt@gmail.com>
2026-01-20 17:57:06 +0100

    solve security problem

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,28 +1,36 @@
+import Image from "next/image";
+
 export default async function Movie({
   params,
 }: {
-  params: Promise<{ id: string }>
+  params: Promise<{ id: string }>;
 }) {
-  const { id } =  await params;
+  const { id } = await params;
 
   const url = `https://api.themoviedb.org/3/movie/${id}`;
 
   const options = {
     method: "GET",
-    
+
     headers: {
       accept: "application/json",
-      Authorization: 
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZWZhYzQxNzJiYThiOWJhMmNmZWU3YTljZDc4NDYzYiIsIm5iZiI6MTc2ODY1ODg4NS43MjYsInN1YiI6IjY5NmI5N2M1NDkyMjc1YjJkNDhmYzViMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.l_2cCpIDlOhemQPaV906w5W8A2_nPoeMuams47v-pMI",
-    }
-  }
+      Authorization:
+        `Bearer ${process.env.TMDB_TOKEN}`
+    },
+  };
 
   const res = await fetch(url, options);
   const movie = await res.json();
 
-  console.log(movie);
-
-  return <div>
-    <img src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}/>
-  </div>
+  return (
+    <div className="relative w-full h-screen">
+      <Image
+        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        alt="Movie Backdrop"
+        fill
+        className="object-cover"
+        priority
+      />
+    </div>
+  );
 }
